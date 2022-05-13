5afc56409f912da7ef63a577da148a24eca92e32
Arthur Kovacs <kovacs.arthur.kraid@gmail.com>
2022-05-13 14:48:24 +0300

    Random generation

--- www/index.js
+++ www/index.js
@@ -2,7 +2,7 @@
 import {memory} from "wasm-game-of-life/wasm_game_of_life_bg";
 import { Universe, Cell } from "wasm-game-of-life";
 
-const CELL_SIZE = 5; // px
+const CELL_SIZE = 10; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
